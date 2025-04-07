667ebf997f0ed5aa1ddc91dbffacb2806f30ead4
Dharamkar Shruthi Shre <dharamkarshruthishre1807@gmail.com>
2025-04-07 18:15:13 +0530

    Create add_long_int.py

---
+++ add_long_int.py
@@ -0,0 +1,18 @@
+
+def aVeryBigSum(ar):
+    # Write your code here
+    res=sum(ar)
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
